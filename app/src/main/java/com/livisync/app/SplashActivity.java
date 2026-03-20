0b05adb3efbe5681d3caef6e7f80671a12e45d88
awais <awaisakhtar205@gmail.com>
2026-03-20 11:23:13 +0500

    added animation in splash, changed colors, added IDs

--- app/src/main/java/com/livisync/app/SplashActivity.java
+++ app/src/main/java/com/livisync/app/SplashActivity.java
@@ -3,6 +3,9 @@ package com.livisync.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,24 +13,25 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
-
+    Animation titleAnim,sloganAnim;
+    TextView title,slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        // Wait 2 seconds then decide where to go
+        titleAnim= AnimationUtils.loadAnimation(this,R.anim.title_anim);
+        title=findViewById(R.id.txtTitle);
+        slogan=findViewById(R.id.txtSlogan);
+        title.startAnimation(titleAnim);
         new Handler().postDelayed(() -> {
             FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
             if (currentUser != null) {
-                // Already logged in → go to main screen
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             } else {
-                // Not logged in → go to login
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
             }
-            finish(); // destroy splash so user cant go back to it
+            finish();
         }, 2000);
     }
 }
